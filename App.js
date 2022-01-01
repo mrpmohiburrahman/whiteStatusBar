2e15088977c00e4a94bee9fea4d6db67ccf0d8a3
MD. MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-01-01 14:24:35 +0600

    implementation so far

--- App.js
+++ App.js
@@ -2,6 +2,58 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+const AuthStack = createNativeStackNavigator();
+const BottomStack = createBottomTabNavigator();
+const HomeStack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+import Home from "app/screens/Home";
+import Notifications from "app/screens/WearNotifications";
+
+import ScanCloth from "app/screens/ScanCloth";
+import MyWardrobe from "app/screens/MyWardrobe";
+
+const HomeStackScreen = () => {
+  // problem occurs with the screens in this navigator
+  return (
+    <HomeStack.Navigator screenOptions={{ headerShown: false }}>
+      <HomeStack.Screen name="Home" component={Home} />
+      <HomeStack.Screen
+        name="ScreenName"
+        component={ComponentToDisplayOnClick}
+      />
+      <HomeStack.Screen
+        name="ScreenName"
+        component={ComponentToDisplayOnClick}
+      />
+    </HomeStack.Navigator>
+  );
+};
+
+// This is Bottom Tab
+const LoggedInNavigator = () => (
+  <BottomStack.Navigator
+    screenOptions={() => ({
+      tabBarStyle: {
+        backgroundColor: "#1B1464",
+        height: 65,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        paddingBottom: 10,
+        paddingTop: 10,
+      },
+      headerShown: false,
+      tabBarLabel: () => {
+        return null;
+      },
+    })}>
+    <BottomStack.Screen name="HomeStack" component={HomeStackScreen} />
+    {/* ... other bottom tabs */}
+  </BottomStack.Navigator>
+);
 
 function HomeScreen() {
   return (
@@ -19,15 +71,12 @@ function SettingsScreen() {
   );
 }
 
-const Tab = createBottomTabNavigator();
-
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+    <NavigationContainer ref={navigationRef}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={LoggedInNavigator} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
